814f84c638e37789e4a93fe5521cd1974721aed5
Didi Abdillah HMDev <didiabdillah@users.noreply.github.com>
2021-11-14 22:11:06 +0700

    install axios package

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/layout/Navbar';
+import Dashboard from './components/layout/Dashboard';
 
 function App() {
   return (
     <div className="App">
-  
+        <Navbar />
+        <div className="container"><Dashboard/></div>
     </div>
   );
 }
